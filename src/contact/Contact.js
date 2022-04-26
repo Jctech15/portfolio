eec7c4e79cdcf0a40cfc96db744665123a24302b
Jctech15 <jyctech15@gmail.com>
2022-04-26 14:10:16 -0700

    continue with media queries

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -5,55 +5,57 @@ export default function Contact() {
   return (
     <div className="Contact">
       <h4>Contact Me!</h4>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim accusamus
-        laboriosam culpa quidem dignissimos? Fugit, ex expedita voluptatibus
-        reprehenderit autem sequi? Provident nam ea voluptate laborum aspernatur
-        at quod nemo!
-      </p>
-      <ul>
-        <li>
-          <a className="react-icons" href="#">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a className="react-icons" href="#">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a className="react-icons" href="#">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a className="react-icons" href="#">
-            <FaTwitter />
-          </a>
-        </li>
-      </ul>
-      <form>
-        <div className="name">
-          <input type="text" placeholder="name" />
-          <label for="name" autocomplete="off"></label>
-        </div>
-        <div className="email">
-          <input type="text" placeholder="email" />
-          <label for="email" autocomplete="off"></label>
-        </div>
-        <div className="message">
-          <textarea
-            id="textarea"
-            name="message"
-            cols="30"
-            rows="10"
-            placeholder="message"
-            autocomplete="off"
-          ></textarea>
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
+      <div className="flexContact">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+          accusamus laboriosam culpa quidem dignissimos? Fugit, ex expedita
+          voluptatibus reprehenderit autem sequi? Provident nam ea voluptate
+          laborum aspernatur at quod nemo!
+        </p>
+        <ul>
+          <li>
+            <a className="react-icons" href="#">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a className="react-icons" href="#">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a className="react-icons" href="#">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a className="react-icons" href="#">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+        <form className="contactForm">
+          <div className="name">
+            <input type="text" placeholder="name" />
+            <label for="name" autocomplete="off"></label>
+          </div>
+          <div className="email">
+            <input type="text" placeholder="email" />
+            <label for="email" autocomplete="off"></label>
+          </div>
+          <div className="message">
+            <textarea
+              id="textarea"
+              name="message"
+              cols="30"
+              rows="10"
+              placeholder="message"
+              autocomplete="off"
+            ></textarea>
+          </div>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -6,19 +6,22 @@ export default function Projects() {
     <div className="Projects">
       <h3>Projects</h3>
       <ul className="projectList">
-        <li>
-          <img className="projectImg" src={Projectimg} alt="projectImg" />
-        </li>
-        <li>
-          <img className="projectImg" src={Projectimg} alt="projectImg" />
-        </li>
-
-        <li className="pleaseMove">
-          <img className="projectImg" src={Projectimg} alt="projectImg" />
-        </li>
-        <li>
-          <img className="projectImg" src={Projectimg} alt="projectImg" />
-        </li>
+        <div className="breakPoint">
+          <li>
+            <img className="projectImg" src={Projectimg} alt="projectImg" />
+          </li>
+          <li>
+            <img className="projectImg" src={Projectimg} alt="projectImg" />
+          </li>
+        </div>
+        <div className="breakPoint">
+          <li>
+            <img className="projectImg" src={Projectimg} alt="projectImg" />
+          </li>
+          <li>
+            <img className="projectImg" src={Projectimg} alt="projectImg" />
+          </li>
+        </div>
       </ul>
     </div>
   );
